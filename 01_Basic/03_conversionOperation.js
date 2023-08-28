0dd3d363dcdfc6151f798ad68565941b71efbd7a
KULDEEP GANGWAR <120949786+Kuldeep8057@users.noreply.github.com>
2023-08-28 13:31:28 +0000

    conversion of datatype

---
+++ 01_Basic/03_conversionOperation.js
@@ -0,0 +1,27 @@
+// let score = "33ab"
+// let score = null
+let score = undefined
+
+
+
+// console.log(typeof(score));
+
+let valueINNumber = Number(score)
+// console.log(typeof valueINNumber);
+// console.log(valueINNumber);
+
+// "33" => 33
+//"33abc" => NaN
+// true => 1; false=>0
+
+// let isLoggedIn = 1
+
+// let isLoggedIn = ""
+let isLoggedIn = "kuldeep"
+
+let booleanLoggedIn = Boolean(isLoggedIn)
+//  console.log(booleanLoggedIn);
+let anyNumber = 33
+let stringNumber = String(anyNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
