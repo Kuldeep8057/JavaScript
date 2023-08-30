a8af67e9dc469b39197fb7f72790bc862579924c
KULDEEP GANGWAR <120949786+Kuldeep8057@users.noreply.github.com>
2023-08-30 01:27:57 +0000

    Number and Math

---
+++ 01_Basic/06_nums_and_math.js
@@ -0,0 +1,39 @@
+const score = 400
+
+const balance = new Number(100)
+// console.log(balance);
+// console.log(score);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed());
+
+// const otherNumber = 23.896
+const otherNumber = 123.896
+
+// console.log(otherNumber.toPrecision(3));
+
+const hundreds = 10000000
+// console.log(hundreds.toLocaleString());
+// console.log(hundreds.toLocaleString('en-IN'));
+
+//+++++++++++++++++++++++++ Math +++++++++++++++++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(-4));  
+// console.log(Math.round(4.6)); 
+// console.log(Math.ceil(4.2)); 
+// console.log(Math.max(4,5,3,8));   
+// console.log(Math.min(4,5,3,8));   
+
+// console.log(Math.random());
+// console.log((Math.random()*10) + 1);
+// console.log(Math.floor(Math.random()*10) + 1);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
+
+
+
+
